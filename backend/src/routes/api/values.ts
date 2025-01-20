fa9ff39d76f934e3ce8eb7ef55905c987960b8f8
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-01-20 10:41:45 +0100

    Support env files

---
+++ backend/src/routes/api/values.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const VALUES: {
+    PORT: string;
+    HOST: string;
+    PEKA_VM_URL: string;
+} = {
+    PORT: process.env.PORT || "3000",
+    HOST: process.env.HOST || "127.0.0.1",
+    PEKA_VM_URL: process.env.PEKA_VM_URL || "https://www.peka.poznan.pl/vm/method.vm"
+};
