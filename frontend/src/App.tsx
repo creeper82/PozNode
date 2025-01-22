445622501083f20767c32249a8c87628bc6376bf
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-01-22 17:45:05 +0100

    Add a dark mode toggle, reorganize page structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import MainScreen from "./screens/MainScreen";
 import { useEffect, useState } from "react";
+import Header from "./components/Header";
 
 function App() {
-  const [isDarkMode, setDarkMode] = useState(true);
+  const [isDarkMode, setDarkMode] = useState((localStorage.getItem("PozNodeDarkMode") || "true") == "true");
 
   useEffect(() => {
     document.body.classList.toggle('dark-theme', isDarkMode);
+    localStorage.setItem("PozNodeDarkMode", isDarkMode ? "true" : "false");
   }, [isDarkMode]);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<MainScreen />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+
+      <BrowserRouter>
+        <Header isDarkMode={isDarkMode} onDarkModeChanged={(m) => { setDarkMode(m == "dark"); }} />
+        <div style={{ padding: "16px" }}>
+          <Routes>
+            <Route path="" element={<MainScreen />} />
+          </Routes>
+        </div>
+
+      </BrowserRouter>
+    </>
   );
 }
 
--- frontend/src/screens/MainScreen.tsx
+++ frontend/src/screens/MainScreen.tsx
@@ -1,9 +1,8 @@
-import Header from "../components/Header";
-
 export default function MainScreen() {
     return (
         <>
-        <Header />
+        <h1>Content!</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed quia voluptates animi reiciendis incidunt blanditiis commodi, nostrum quibusdam ipsam nemo id similique eos, molestiae, voluptate quae inventore recusandae reprehenderit adipisci.</p>
         </>
     )
 }
