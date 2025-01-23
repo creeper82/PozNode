4281d1ba85104826ea4a41fd8ef3b1735ce9e875
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-01-23 12:42:51 +0100

    Move values into src, change port to 5000

--- backend/src/values.ts
+++ backend/src/values.ts
@@ -6,7 +6,7 @@ export const VALUES: {
     HOST: string;
     PEKA_VM_URL: string;
 } = {
-    PORT: process.env.PORT || "3000",
+    PORT: process.env.PORT || "5000",
     HOST: process.env.HOST || "127.0.0.1",
     PEKA_VM_URL: process.env.PEKA_VM_URL || "https://www.peka.poznan.pl/vm/method.vm"
 };
