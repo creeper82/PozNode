1abf8480c284ee3fb5a2a703808874215e37286b
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-01-24 08:16:33 +0100

    Add dividers

--- frontend/src/components/CompactDeparturesCard.tsx
+++ frontend/src/components/CompactDeparturesCard.tsx
@@ -1,6 +1,7 @@
 import Departure from "../types/Departure";
 import CompactDeparture from "./CompactDeparture";
 import style from "../styles/compact_departures_card.module.scss";
+import Divider from "./Divider";
 
 export default function CompactDeparturesCard({ title, departures, maxHeight = "300px" }: { title: string, departures: Departure[]; maxHeight?: string; }) {
     return (
@@ -8,7 +9,10 @@ export default function CompactDeparturesCard({ title, departures, maxHeight = "
             <p className={style.title}>{title}</p>
             <div className={style.departures} style={{ maxHeight: maxHeight }}>
                 {departures.map(departure =>
-                    <CompactDeparture key={`${departure.line}_${departure.direction}_${departure.minutes}`} line={departure.line} direction={departure.direction} minutes={departure.minutes} live={departure.realTime} />
+                    <>
+                        <CompactDeparture key={`${departure.line}_${departure.direction}_${departure.minutes}`} line={departure.line} direction={departure.direction} minutes={departure.minutes} live={departure.realTime} />
+                        <Divider />
+                    </>
                 )}
             </div>
 
---
+++ frontend/src/components/Divider.tsx
@@ -0,0 +1,7 @@
+import style from "../styles/divider.module.scss";
+
+export default function Divider() {
+    return (
+        <div className={style.root}></div>
+    )
+}
