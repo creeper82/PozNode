cae08c0090c4aae5ad20000c3d39900b0f659d7b
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-01-26 10:36:17 +0100

    Add div props in ChipList

--- frontend/src/components/ChipList.tsx
+++ frontend/src/components/ChipList.tsx
@@ -1,8 +1,14 @@
 import { ReactNode } from "react";
 import style from "../styles/chip_list.module.scss";
 
-export default function ChipList({ children }: { children: ReactNode; }) {
+export interface ChipListProps extends React.HTMLAttributes<HTMLDivElement> {
+    children: ReactNode;
+}
+
+export default function ChipList(props: ChipListProps) {
+    const { children, ...rest } = props;
+
     return (
-        <div className={style.root}>{children}</div>
+        <div className={style.root} {...rest}>{children}</div>
     );
 }
