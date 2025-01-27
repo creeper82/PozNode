ab7f415abcbcc6a84a5f5caadd00d44eee07ce92
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-01-27 10:55:16 +0100

    Display announcements

--- frontend/src/components/DeparturesCard.tsx
+++ frontend/src/components/DeparturesCard.tsx
@@ -2,10 +2,16 @@ import Departure from "../types/Departure";
 import style from "../styles/departures_card.module.scss";
 import Divider from "./Divider";
 import DepartureComponent from "./Departure";
+import Announcement from "../types/Announcement";
+import ExpandableAnnouncements from "./ExpandableAnnouncements";
 
-export default function DeparturesTable({ departures }: { departures: Departure[]; }) {
+export default function DeparturesTable({ departures, announcements = [] }: { departures: Departure[]; announcements?: Announcement[]; }) {
     return (
         <div className={style.root}>
+            {announcements.length > 0 &&
+                <ExpandableAnnouncements announcements={announcements} />
+            }
+
             <div className={style.departures}>
                 {departures.map(departure =>
                     <div key={`${departure.line}_${departure.direction}_${departure.minutes}`}>
