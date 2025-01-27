cb3f713865dce4bde36af03bd78a001216c03591
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-01-27 11:03:20 +0100

    Set the main screen's title

--- frontend/src/screens/main/MainScreen.tsx
+++ frontend/src/screens/main/MainScreen.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import SearchBar from "../../components/SearchBar";
 import FavoritesSection from "./FavoritesSection";
 
 export default function MainScreen() {
+    useEffect(() => {
+        document.title = "PozNode";
+    }, []);
+    
     return (
         <div className="content">
             <SearchBar />
